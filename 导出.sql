437cb85ee168198d138d66ba074672231c1c220d
chenhu <chenhu1008@gmail.com>
2015-04-13 22:20:02 +0800

    delete sql

--- 导出.sql
+++
@@ -1,4 +0,0 @@
-REM INSERTING into M_FUNCTION_URL_FILTER
-SET DEFINE OFF;
-Insert into M_FUNCTION_URL_FILTER (FUNCID,SYSTEMID,MATCHTYPE,URLPATTERN,MATCHRESULT,PRIORITY,DESCRIPTION) values ('006003001',1,0,'/fixedline/online/enterpriseuser/massquery.jsp',null,0,'jsp filter');
-Insert into M_FUNCTION_URL_FILTER (FUNCID,SYSTEMID,MATCHTYPE,URLPATTERN,MATCHRESULT,PRIORITY,DESCRIPTION) values ('006003001',1,0,'/fixedline/online/enterpriseuser/query_show.action',null,0,'action filter');
